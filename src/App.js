a5b8baa6256257891ad2a2b6a091f331d9735b5b
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-04-24 00:18:55 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/groups'); ///
+    const body = await response.json();
+    this.setState({groups: body, isLoading: false});
+  }
+
   render() {
+
+    const {groups, isLoading} = this.state;
+
+    if(isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="UniCMS">
+              <h2>UniCMSList</h2>
+            {groups.map(group =>
+              <div key={group.id}>
+                {group.name}
+              </div>
+
+            )}
+          </div>
+
         </header>
       </div>
     );
